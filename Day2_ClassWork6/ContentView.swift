8d934f10bc3d24da2021c1a7f48839e2045ad0e3
Ali Mohammad Baqer <boobaqer@icloud.com>
2020-05-04 23:23:32 +0300

    Initial Commit

---
+++ Day2_ClassWork6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day2_ClassWork6
+//
+//  Created by Ali-mac Baqer on 5/4/20.
+//  Copyright © 2020 AliBaqer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
